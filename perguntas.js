02d2f822f0d2a689d4100999434ae30a44cf1afe
mimih2007 <milenalkd9@gmail.com>
2024-11-18 21:24:33 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'comida',
+    'qual a melhor lasanha, molho branco ou bolonhesa?',
+    'bolonhesa'
+)
+
+criaCartao( 
+    'esporte',
+    'quantos jogadores tem um time de futebol?',
+    '11'
+)
+
+criaCartao(
+    'comida',
+    'qual a cor do quindim?',
+    'amarelo'
+)
+
+criaCartao(
+    'esporte',
+    'qual o melhor time do mundo',
+    'corinthians'
+)
